89627757614725d5e774855b13d2cbb9e709f298
Mukesh Yadav <72705190+MK-Brightpath-Academy@users.noreply.github.com>
2026-08-20 22:09:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/First and Second Smallests/first-and-second-smallests.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public ArrayList<Integer> minAnd2ndMin(int[] arr) {
+        int first = Integer.MAX_VALUE;
+        int second = Integer.MAX_VALUE;
+
+        for (int num : arr) {
+            if (num < first) {
+                second = first;
+                first = num;
+            } 
+            else if (num > first && num < second) {
+                second = num;
+            }
+        }
+
+        ArrayList<Integer> result = new ArrayList<>();
+
+        if (second == Integer.MAX_VALUE) {
+            result.add(-1);
+        } else {
+            result.add(first);
+            result.add(second);
+        }
+
+        return result;
+    }
+}
